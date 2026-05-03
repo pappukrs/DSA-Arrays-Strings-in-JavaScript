4f800e1cf78fcde68aa71580b88aa379ac970ec3
pappukrs <digitalpappu101@gmail.com>
2026-05-03 17:25:30 +0530

    numdecode

---
+++ numDecode.js
@@ -0,0 +1,11 @@
+function numDecodings(s) {
+    if (!s || s[0] === '0') return 0;
+    const n = s.length, dp = Array(n + 1).fill(0);
+    dp[0] = 1; dp[1] = 1;
+    for (let i = 2; i <= n; i++) {
+        if (s[i - 1] !== '0') dp[i] += dp[i - 1];
+        const two = Number(s.slice(i - 2, i));
+        if (two >= 10 && two <= 26) dp[i] += dp[i - 2];
+    }
+    return dp[n];
+}
