d92c34536c5aebecbcf396c0b78967d7ca250bda
pappukrs <digitalpappu101@gmail.com>
2026-05-11 09:53:12 +0530

    pal

---
+++ pal.js
@@ -0,0 +1,7 @@
+function longestPalindrome(s) {
+    let res = '';
+    for (let i = 0; i = 0 && rres.length) res = s.slice(l, r + 1);
+    for (let l = i, r = i + 1; l >= 0 && rres.length) res = s.slice(l, r + 1);
+}
+return res;
+}
